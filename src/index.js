21d2a9ead55120bcd34bd4f3a0ddd04bb2c216d4
atsuc1973 <atsu1973@gmail.com>
2021-06-15 14:31:28 +0900

    implement events iundex

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 import './index.css';
 import reducer from './reducers'
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import EventsIndex from './components/events_index';
+import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <EventsIndex />
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+registerServiceWorker();
